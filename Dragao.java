ca1a1921b448c4522d00ca06f74d1dc75ba2616f
AnaPaulaNSilva <130613321+AnaPaulaNSilva@users.noreply.github.com>
2023-05-11 21:42:10 -0300

    atividade Jogo2

---
+++ Dragao.java
@@ -0,0 +1,27 @@
+public class Dragao {
+	private int x;
+	private int y;
+
+	@Override
+	public void mover(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
